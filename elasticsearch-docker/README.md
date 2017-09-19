8fc857cb1e3c03d982d120fa61c43311ca44eb89
nakaearth <naka5313@gmail.com>
2017-09-19 22:27:27 +0900

    最新をmerge

---
+++ elasticsearch-docker/README.md
@@ -0,0 +1,18 @@
+## Description
+
+This is the official Elasticsearch image created by Elastic Inc.
+
+Documentation can be found on the [Elastic web site](https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html).
+
+## Supported Docker versions
+
+The images have been tested on Docker 1.12.
+
+## Contributing, issues and testing
+
+This image is built on top of [elasticsearch-alpine-base](https://github.com/elastic/elasticsearch-alpine-base) and is based on [alpine:latest](https://hub.docker.com/_/alpine/).
+The complete set of Elasticsearch tests is [regularly executed](https://elasticsearch-ci.elastic.co/view/Elasticsearch/job/elastic+elasticsearch+master+dockeralpine-periodic/) against it.
+
+To report issues, please open an issue in [GitHub](https://github.com/elastic/elasticsearch-docker/issues).
+
+To contribute, please fork and create a PR. Please ensure that tests pass by running `make`. Tests require [docker-compose](https://docs.docker.com/compose/install/).
---
+++ elasticsearch-docker/tests/test_xpack_basic_index_crud.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+from es_acceptance import docker_stack, docker_xpack_stack_uninitialized
+from es_acceptance import cluster_health, query_all, delete_index, create_index, wait_for_cluster_health, change_default_elastic_password, os_stats
+from pytest import fixture
+
+# change default x-pack password; cluster state should be yellow after that
+def test_password_change(docker_xpack_stack_uninitialized):
+    status = change_default_elastic_password()
+    assert 200 == status
+
+
+def test_create_basic_index(docker_stack):
+    hits = query_all()['hits']['hits']
+    assert "lorem" in hits[0]['_source'].values()
+    assert "ipsum" in hits[0]['_source'].values()
+
+
+def test_delete_basic_index(docker_stack):
+    delete_result = delete_index()
+    assert {'acknowledged': True} == delete_result
+
+
+def test_fail_search_on_nonexistent_index(docker_stack):
+    delete_index()
+    error_root_cause = query_all()['error']['root_cause']
+    assert "no such index" == error_root_cause[0]['reason']
+    assert "simpleindex" == error_root_cause[0]['index']
+
+
+def test_cluster_health_after_crud(docker_stack):
+    health = cluster_health()
+    # health will be yellow on single node cluster after data has been inserted
+    if health['number_of_nodes'] == 1:
+        wait_for_cluster_health('yellow')
+        # TODO convert ^^ to an assert
+    else:
+        wait_for_cluster_health('green')
+
+    delete_index()
+
+
+def test_cgroup_os_stats(docker_stack):
+    nodes_os_stats = os_stats()
+    # Elasticsearch should be capable of returning cgroup stats for all nodes
+    for nodenane, node_os_stats in nodes_os_stats["nodes"].iteritems():
+        assert 'cgroup' in node_os_stats['os']
+        assert 'cpu' in node_os_stats['os']['cgroup']
+        assert 'cpuacct' in node_os_stats['os']['cgroup']
